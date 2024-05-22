e93e83e43890a0d8ab35a50423fe651798ae4dd2
vinaykn <vinaykn@users.noreply.github.com>
2024-05-22 18:35:34 +0530

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+export default {
+  // ...
+  url: 'https://vinaykn.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'vinaykn.github.io',
+  organizationName: 'vinaykn',
+  trailingSlash: false,
+  // ...
+};
